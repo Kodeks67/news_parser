9d99810f0d33e007880d812e53bea08576911b24
zaur <zaur.cherchesov@mail.ru>
2022-09-18 12:42:58 +0300

    added main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+import datetime
+
+URL_TEMPLATE = "https://lenta.ru/parts/news/"
+FILE_NAME = "test.csv"
+
+
+def parse(url=URL_TEMPLATE):
+    r = requests.get(url)
+    soup = bs(r.text, "html.parser")
+    news_collections = soup.find_all('a', class_='card-full-news _parts-news')
+    result_list = []
+    now = datetime.date.today()
+
+    for element in news_collections:
+        elem = element.h3
+        div_elem = element.div
+        time_news = div_elem.time.text
+        if time_news.find(',') != -1:
+            time_news = time_news + ' ' + now
+        news_str = elem.text + ' ' + time_news
+        result_list.append(news_str)
+        print(news_str)
+        # result_list['news'].append(elem.text)
+        # result_list['date'].append(time_news)
+    #   print(news)
+    # news_data_list.append(news)
+
+    print(result_list)
+
+df = pd.DataFrame(data=parse())
+df.to_csv(FILE_NAME)
+
+parse(URL_TEMPLATE)
